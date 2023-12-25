d7ac049d84dc29e5ca1b839b5998603b6e46d3b5
sangmesh panchal <you@example.com>
2023-12-25 15:45:43 +0530

    Section

---
+++ qtify/src/Components/Section/Section.jsx
@@ -0,0 +1,46 @@
+import React, {useState} from "react";
+import Card from "../Card/Card";
+import styles from "./Section.module.css"
+import { CircularProgress } from "@mui/material";
+
+
+export default function Section({title, data, type}){
+    const [carouselData, setCarouselData] = useState(false);
+    // console.log(type)
+
+    const handleToggle = () => {
+        setCarouselData(prevState => !prevState)
+    }
+
+    return (
+        <div>
+        <div className={styles.header}>
+            <h3>{title}</h3>
+            <h4 className={styles.toggleText} onClick={handleToggle}>{!carouselData ? "Collapse All" : "Show All"}</h4>
+        </div>
+
+       {data.length === 0? (
+            <CircularProgress />
+        ) : (
+            <div className={styles.cardWrapper}>
+                {!carouselData ? 
+                    (
+                    <div className={styles.wrapper}>
+                        {data.map((ele) => <Card data={ele} type={type} />)}
+                    </div>
+                    ) 
+                    :
+                    (
+                        // <Carousel 
+                        //     data={data}
+                        //     renderedComponent={(data) => <Card data={data} type={type} />}
+                        // />
+                        //carousel
+                        null
+                    )
+                }            
+            </div>
+        )} 
+        </div>
+    )
+}
