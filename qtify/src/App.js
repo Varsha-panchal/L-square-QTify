f5ee7937d392388298ef727eece08e75f2a328cc
sangmesh panchal <you@example.com>
2023-12-25 14:22:36 +0530

    initial commit

--- qtify/src/App.js
+++ qtify/src/App.js
@@ -1,37 +1,39 @@
-import { useEffect, useState } from "react";
+// import { useEffect, useState } from "react";
 import React from "react"
 import Navbar from "./Components/Navbar/Navbar";
 // import Hero from "../Components/Hero/Hero"
-import { Outlet } from "react-router-dom";
-import { fetchNewAlbums, fetchSongs, fetchTopAlbums } from "./Api/Api";
+// import { Outlet } from "react-router-dom";
+// import { fetchNewAlbums, fetchSongs, fetchTopAlbums } from "./Api/Api";
 
 function App() {
-  const [searchData, setSearchData] = useState();
-  const[data, setData] = useState({});
+  // const [searchData, setSearchData] = useState();
+  // const[data, setData] = useState({});
 
-  const generateData = (key, source) => {
-    source().then((value) => {
-      setData((prevData) => {
-        return {...prevData, [key]: value}
-      })
-    })
-  }
+  // const generateData = (key, source) => {
+  //   source().then((value) => {
+  //     setData((prevData) => {
+  //       return {...prevData, [key]: value}
+  //     })
+  //   })
+  // }
 
-  useEffect(() => {
-    generateData("topAlbums", fetchTopAlbums)
-    generateData("newAlbums", fetchNewAlbums)
-    generateData("songs", fetchSongs)
+  // useEffect(() => {
+  //   generateData("topAlbums", fetchTopAlbums)
+  //   generateData("newAlbums", fetchNewAlbums)
+  //   generateData("songs", fetchSongs)
 
-  }, [])
+  // }, [])
 
-  const { topAlbums=[], newAlbums=[], songs=[] } = data;
+  // const { topAlbums=[], newAlbums=[], songs=[] } = data;
 
 
   return (
     <>
-      <Navbar searchData={[...topAlbums, ...newAlbums]} />
+      {/* <Navbar searchData={[...topAlbums, ...newAlbums]} /> */}
+      <Navbar />
+
      {/* placeholder component (replaced with childcomponent) */}
-     <Outlet context={{data: {topAlbums, newAlbums, songs}}}/> 
+     {/* <Outlet context={{data: {topAlbums, newAlbums, songs}}}/>  */}
     </>
   );
 }
