030934c6a95e4c9549837bb097fc38de527e55ba
sangmesh panchal <you@example.com>
2023-12-31 19:30:19 +0530

    Filters

--- qtify/src/Components/Section/Section.jsx
+++ qtify/src/Components/Section/Section.jsx
@@ -1,24 +1,50 @@
-import React, {useState} from "react";
+import React, {useEffect, useState} from "react";
 import Card from "../Card/Card";
 import styles from "./Section.module.css"
 import { CircularProgress } from "@mui/material";
-import Carousel from "../Carousel/Carousel";
+import Carousel from "../Carousel/Carousel"
+import Filters from "../Filters/Filters"
 
+export default function Section({title, data, filterSource, type}){
 
-export default function Section({title, data, type}){
-    const [carouselData, setCarouselData] = useState(true);
-    //  console.log(type)
+    const [filters, setFilters] = useState([{key: "all", label: "all"}]);
+    const [selectedFilterIndex, setSelectedFilterIndex] = useState(0);
+    const [carouselData, setCarouselData] = useState(false);
+    
 
     const handleToggle = () => {
         setCarouselData(prevState => !prevState)
     }
 
+    useEffect(() => {
+        if(filterSource){
+            filterSource().then((response) => {
+                const {data} = response;
+                setFilters([...filters, ...data])
+            })
+        }
+    }, [])
+    // console.log(filters)
+
+    const showFilters = filters.length > 1;
+    const cardsToRender = data.filter((card) => 
+        showFilters && selectedFilterIndex !== 0 ?
+        card.genre.key === filters[selectedFilterIndex].key : card)
+
     return (
         <div>
         <div className={styles.header}>
             <h3>{title}</h3>
-            <h4 className={styles.toggleText} onClick={handleToggle}>{!carouselData ? "Collapse All" : "Show All"}</h4>
+           <h4 className={styles.toggleText} onClick={handleToggle}>{!carouselData ? "Collapse All" : "Show All"}</h4>
         </div>
+        {showFilters && (
+            <div className={styles.filterWrapper}>
+                <Filters 
+                    filters={filters}
+                    selectedFilterIndex={selectedFilterIndex}
+                    setSelectedFilterIndex={setSelectedFilterIndex}
+                />
+            </div>)}
 
        {data.length === 0? (
             <CircularProgress />
@@ -27,18 +53,15 @@ export default function Section({title, data, type}){
                 {!carouselData ? 
                     (
                     <div className={styles.wrapper}>
-                        {data.map((ele) => <Card data={ele} type={type} />)}
+                        {cardsToRender.map((ele) => <Card data={ele} type={type} />)}
                     </div>
                     ) 
                     :
                     (
-                         <Carousel
-                            data={data}
+                        <Carousel 
+                            data={cardsToRender}
                             renderedComponent={(data) => <Card data={data} type={type} />}
-                         />
-                         
-                    
-                        
+                        />
                     )
                 }            
             </div>
--- qtify/src/Pages/Homepage/Homepage.jsx
+++ qtify/src/Pages/Homepage/Homepage.jsx
@@ -3,10 +3,12 @@ import styles from "./Homepage.module.css"
 import { useOutletContext } from "react-router-dom";
 import Hero from "../../Components/Hero/Hero"
 import Section from "../../Components/Section/Section";
+import { fetchFilters } from "../../Api/Api";
 
 function Homepage(){
     const {data} = useOutletContext();
     const { topAlbums, newAlbums, songs } = data;
+
 
     return (
         <>
@@ -15,7 +17,11 @@ function Homepage(){
             <Section title="Top Albums" data={topAlbums} type="album"/>
             
             <Section title="New Albums" data={newAlbums} type="album"/>
-            {/* <Section title="Songs" data={topAlbums} type="song"/> */}
+            <Section
+                title="Songs" 
+                data={songs}
+                type="song"
+                filterSource={fetchFilters}/>
         </div>
         </>
     )
